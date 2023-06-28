9267c472ad55c909d5cb24a6c18ddaed8b9d45eb
Roman Shkuruoiy <shkurupiy.roman@gmail.com>
2023-06-28 11:12:35 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
 // information;
 // information;
+        // information;
+        // information;
+        // information;
         System.out.println("Hello world!");
     }
 }
